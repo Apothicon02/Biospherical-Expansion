4cdf4b46f1de242cbc75dababcb8f54d16ed9f86
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2023-09-19 00:33:19 -0400

    - Added dirt / mycelium / podzol / grass / cave vines / netherrack brightness / saturation tinting.

--- src/main/java/com/Apothic0n/BiosphericalExpansion/core/events/ModEvents.java
+++ src/main/java/com/Apothic0n/BiosphericalExpansion/core/events/ModEvents.java
@@ -21,19 +21,54 @@ public class ModEvents {
     @SubscribeEvent
     public static void onBlockColors(RegisterColorHandlersEvent.Block event) {
         event.register((blockState, blockAndTintGetter, blockPos, tint) -> {
-            int x = blockPos.getX();
-            int z = blockPos.getZ();
-            int color = -328966;
-            double saturate = -Mth.clamp((SATURATION_NOISE.getValue(x * 0.05, z * 0.01, false) + 0.33) * 0.33, -0.33, 0.33);
-            double temperature = (Minecraft.getInstance().level.getBiome(blockPos).get().getBaseTemperature() - 1) * 0.1;
-            float red = (float) ((Mth.clamp(FastColor.ABGR32.red(color), 1, 255) / 255) + temperature);
-            float green = (float) Mth.clamp(FastColor.ABGR32.green(color), 1, 255) / 255;
-            float blue = (float) ((Mth.clamp(FastColor.ABGR32.blue(color), 1, 255) / 255) - temperature);
-            float gray = (red + green + blue) / (3);
-            return FastColor.ABGR32.color(FastColor.ABGR32.alpha(color),
-                    (int) (Mth.clamp(red + (gray - red) * saturate, 0, 1) * 255),
-                    (int) (Mth.clamp(green + (gray - green) * saturate, 0, 1) * 255),
-                    (int) (Mth.clamp(blue + (gray - blue) * saturate, 0, 1) * 255));
-        }, Blocks.STONE, Blocks.STONE_STAIRS, Blocks.STONE_SLAB, Blocks.COAL_ORE, Blocks.IRON_ORE, Blocks.LAPIS_ORE, Blocks.REDSTONE_ORE, Blocks.GOLD_ORE, Blocks.DIAMOND_ORE, Blocks.DEEPSLATE, Blocks.DRIPSTONE_BLOCK, Blocks.POINTED_DRIPSTONE);
+            if (blockPos != null) {
+                int x = blockPos.getX();
+                int z = blockPos.getZ();
+                int color = -328966;
+                double saturate = Mth.clamp(SATURATION_NOISE.getValue(x * 0.077, z * 0.09, false) * 0.33, -0.03, 0.03) + 1;
+                double brighten = Mth.clamp(BRIGHTNESS_NOISE.getValue(x * 0.05, z * 0.01, false), -0.5, 0.5) + 0.75;
+                float red = (float) Mth.clamp(FastColor.ABGR32.red(color), 1, 255) / 255;
+                float green = (float) Mth.clamp(FastColor.ABGR32.green(color), 1, 255) / 255;
+                float blue = (float) Mth.clamp(FastColor.ABGR32.blue(color), 1, 255) / 255;
+                float gray = (float) ((red + green + blue) / (3 + brighten));
+                return FastColor.ABGR32.color(FastColor.ABGR32.alpha(color),
+                        (int) (Mth.clamp((red + (gray - red)) * saturate, 0, 1) * 255),
+                        (int) (Mth.clamp((green + (gray - green)) * saturate, 0, 1) * 255),
+                        (int) (Mth.clamp((blue + (gray - blue)) * saturate, 0, 1) * 255));
+            } else {
+                return -328966;
+            }
+        },
+            Blocks.DIRT, Blocks.ROOTED_DIRT, Blocks.COARSE_DIRT, Blocks.DIRT_PATH, Blocks.PODZOL, Blocks.MYCELIUM, Blocks.CAVE_VINES, Blocks.CAVE_VINES_PLANT, Blocks.NETHERRACK, Blocks.GRASS, Blocks.TALL_GRASS);
+
+        event.register((blockState, blockAndTintGetter, blockPos, tint) -> {
+            if (blockPos != null) {
+                int x = blockPos.getX();
+                int z = blockPos.getZ();
+                int color = -328966;
+                double temperature = (Minecraft.getInstance().level.getBiome(blockPos).get().getBaseTemperature() - 1) * 0.1;
+                double saturate = Mth.clamp(SATURATION_NOISE.getValue(x * 0.1, z * 0.1, false) * 0.33, -0.03, 0.03)+1;
+                float red = (float) Mth.clamp(FastColor.ABGR32.red(color), 1, 255)/255;
+                float green = (float) Mth.clamp(FastColor.ABGR32.green(color), 1, 255)/255;
+                float blue = (float) Mth.clamp(FastColor.ABGR32.blue(color), 1, 255)/255;
+                float gray = (red+green+blue)/(3);
+                return FastColor.ABGR32.color(FastColor.ABGR32.alpha(color),
+                        (int) (Mth.clamp(((red + (gray - red)) * saturate) + temperature, 0, 1) * 255),
+                        (int) (Mth.clamp((green + (gray - green)) * saturate, 0, 1) * 255),
+                        (int) (Mth.clamp(((blue + (gray - blue)) * saturate) - temperature, 0, 1) * 255));
+            } else {
+                return -328966;
+            }
+        },
+                Blocks.STONE, Blocks.STONE_STAIRS, Blocks.STONE_SLAB,
+                Blocks.STONE_BRICKS, Blocks.STONE_BRICK_STAIRS, Blocks.STONE_BRICK_SLAB, Blocks.STONE_BRICK_WALL, Blocks.CRACKED_STONE_BRICKS, Blocks.INFESTED_CRACKED_STONE_BRICKS,
+                Blocks.MOSSY_STONE_BRICKS, Blocks.MOSSY_STONE_BRICK_STAIRS, Blocks.MOSSY_STONE_BRICK_SLAB, Blocks.MOSSY_STONE_BRICK_WALL, Blocks.INFESTED_MOSSY_STONE_BRICKS,
+                Blocks.COAL_ORE, Blocks.IRON_ORE, Blocks.LAPIS_ORE, Blocks.REDSTONE_ORE, Blocks.GOLD_ORE, Blocks.DIAMOND_ORE,
+                Blocks.DEEPSLATE, Blocks.INFESTED_DEEPSLATE,
+                Blocks.DEEPSLATE_BRICKS, Blocks.DEEPSLATE_BRICK_SLAB, Blocks.DEEPSLATE_BRICK_STAIRS, Blocks.DEEPSLATE_BRICK_WALL, Blocks.CRACKED_DEEPSLATE_BRICKS,
+                Blocks.DEEPSLATE_TILES, Blocks.DEEPSLATE_TILE_STAIRS, Blocks.DEEPSLATE_TILE_SLAB, Blocks.DEEPSLATE_TILE_WALL, Blocks.CRACKED_DEEPSLATE_TILES,
+                Blocks.POLISHED_DEEPSLATE, Blocks.POLISHED_DEEPSLATE_STAIRS, Blocks.POLISHED_DEEPSLATE_SLAB, Blocks.POLISHED_DEEPSLATE_WALL,
+                Blocks.DRIPSTONE_BLOCK, Blocks.POINTED_DRIPSTONE,
+                Blocks.DEEPSLATE_COAL_ORE, Blocks.DEEPSLATE_IRON_ORE, Blocks.DEEPSLATE_LAPIS_ORE, Blocks.DEEPSLATE_REDSTONE_ORE, Blocks.DEEPSLATE_GOLD_ORE, Blocks.DEEPSLATE_DIAMOND_ORE);
     }
 }
