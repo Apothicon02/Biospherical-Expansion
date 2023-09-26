bb24c1485b77c0dd52512a0f9312dfb0a0201a4a
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2023-09-25 22:11:25 -0400

    - Added mod icon.

--- src/main/java/com/Apothic0n/BiosphericalExpansion/core/objects/BioxBlocks.java
+++ src/main/java/com/Apothic0n/BiosphericalExpansion/core/objects/BioxBlocks.java
@@ -5,6 +5,8 @@ import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.predicate.BlockStatePredicate;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.material.PushReaction;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,6 +14,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 import static net.minecraft.world.level.block.Blocks.AMETHYST_CLUSTER;
+import static net.minecraft.world.level.block.Blocks.MANGROVE_ROOTS;
 
 public final class BioxBlocks {
     private BioxBlocks() {}
@@ -19,7 +22,7 @@ public final class BioxBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, BiosphericalExpansion.MODID);
 
     public static final RegistryObject<Block> AMETHYST_VINES = BLOCKS.register("amethyst_vines", () ->
-            new com.Apothic0n.BiosphericalExpansion.core.objects.AmethystVinesBlock(BlockBehaviour.Properties.copy(AMETHYST_CLUSTER)
+            new AmethystVinesBlock(BlockBehaviour.Properties.copy(AMETHYST_CLUSTER)
                     .randomTicks().strength(0.2F).noCollission().sound(SoundType.MEDIUM_AMETHYST_BUD)));
 
     public static final RegistryObject<Block> AMETHYST_VINES_PLANT = BLOCKS.register("amethyst_vines_plant", () ->
@@ -35,9 +38,9 @@ public final class BioxBlocks {
                     .replaceable().noCollission().strength(0.2F).sound(SoundType.GLOW_LICHEN).lightLevel(brightness -> {return 7;}).ignitedByLava().pushReaction(PushReaction.DESTROY)));
 
     public static void fixBlockRenderLayers() {
-            ItemBlockRenderTypes.setRenderLayer(AMETHYST_VINES.get(), RenderType.cutout());
-            ItemBlockRenderTypes.setRenderLayer(AMETHYST_VINES_PLANT.get(), RenderType.cutout());
-            ItemBlockRenderTypes.setRenderLayer(GLOWING_AMETHYST.get(), RenderType.cutout());
-            ItemBlockRenderTypes.setRenderLayer(AQUATIC_LICHEN.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AMETHYST_VINES.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AMETHYST_VINES_PLANT.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(GLOWING_AMETHYST.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AQUATIC_LICHEN.get(), RenderType.cutout());
     }
 }
