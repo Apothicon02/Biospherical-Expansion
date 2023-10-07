1acc4c4eb5a05d95bc1930eb04159540eb0ba82f
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2023-10-07 17:16:57 -0400

    - Recreated the Birch Forest biome. - Recreated the Old Growth Birch Forest biome.

--- src/main/java/com/Apothic0n/BiosphericalExpansion/api/biome/BioxSurfaceRuleData.java
+++ src/main/java/com/Apothic0n/BiosphericalExpansion/api/biome/BioxSurfaceRuleData.java
@@ -13,6 +13,8 @@ import net.minecraft.world.level.levelgen.synth.NormalNoise;
 public class BioxSurfaceRuleData {
     private static final SurfaceRules.RuleSource COARSE_DIRT = makeStateRule(Blocks.COARSE_DIRT);
     private static final SurfaceRules.RuleSource MOSS_BLOCK = makeStateRule(Blocks.MOSS_BLOCK);
+    private static final SurfaceRules.RuleSource DIORITE = makeStateRule(Blocks.DIORITE);
+
     public static final ResourceKey<NormalNoise.NoiseParameters> SURFACE_VARIATION = ResourceKey.create(Registries.NOISE, new ResourceLocation("eco","surface_variation"));
     public static final ResourceKey<NormalNoise.NoiseParameters> LESS_SURFACE_VARIATION = ResourceKey.create(Registries.NOISE, new ResourceLocation("biox","less_surface_variation"));
 
@@ -22,7 +24,6 @@ public class BioxSurfaceRuleData {
                         SurfaceRules.ifTrue(SurfaceRules.yStartCheck(VerticalAnchor.absolute(61), 0),
                                 SurfaceRules.ifTrue(SurfaceRules.noiseCondition(SURFACE_VARIATION, 0.069D, 0.18D), COARSE_DIRT)
                         )));
-
         SurfaceRules.RuleSource lessCoarseDirt = SurfaceRules.sequence(
                 SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR,
                         SurfaceRules.ifTrue(SurfaceRules.yStartCheck(VerticalAnchor.absolute(61), 0),
@@ -33,12 +34,19 @@ public class BioxSurfaceRuleData {
                         SurfaceRules.ifTrue(SurfaceRules.yStartCheck(VerticalAnchor.absolute(61), 0),
                                 SurfaceRules.ifTrue(SurfaceRules.noiseCondition(LESS_SURFACE_VARIATION, 0.0370D, 0.23D), MOSS_BLOCK)
                         )));
+        SurfaceRules.RuleSource lessDiorite = SurfaceRules.sequence(
+                SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR,
+                        SurfaceRules.ifTrue(SurfaceRules.yStartCheck(VerticalAnchor.absolute(61), 0),
+                                SurfaceRules.ifTrue(SurfaceRules.noiseCondition(LESS_SURFACE_VARIATION, 0.0370D, 0.23D), DIORITE)
+                        )));
 
         return SurfaceRules.sequence(
                 SurfaceRules.ifTrue(SurfaceRules.isBiome(Biomes.FOREST), coarseDirt),
                 SurfaceRules.ifTrue(SurfaceRules.isBiome(Biomes.TAIGA), SurfaceRules.sequence(lessCoarseDirt, lessMoss)),
                 SurfaceRules.ifTrue(SurfaceRules.isBiome(Biomes.OLD_GROWTH_PINE_TAIGA), SurfaceRules.sequence(lessCoarseDirt, lessMoss)),
-                SurfaceRules.ifTrue(SurfaceRules.isBiome(Biomes.OLD_GROWTH_SPRUCE_TAIGA), SurfaceRules.sequence(lessCoarseDirt, lessMoss)));
+                SurfaceRules.ifTrue(SurfaceRules.isBiome(Biomes.OLD_GROWTH_SPRUCE_TAIGA), SurfaceRules.sequence(lessCoarseDirt, lessMoss)),
+                SurfaceRules.ifTrue(SurfaceRules.isBiome(Biomes.BIRCH_FOREST), lessDiorite),
+                SurfaceRules.ifTrue(SurfaceRules.isBiome(Biomes.OLD_GROWTH_BIRCH_FOREST), lessDiorite));
     }
 
     private static SurfaceRules.RuleSource makeStateRule(Block block) {
--- src/main/java/com/Apothic0n/BiosphericalExpansion/api/biome/features/BioxFeatureRegistry.java
+++ src/main/java/com/Apothic0n/BiosphericalExpansion/api/biome/features/BioxFeatureRegistry.java
@@ -1,13 +1,20 @@
 package com.Apothic0n.BiosphericalExpansion.api.biome.features;
 
 import com.Apothic0n.BiosphericalExpansion.BiosphericalExpansion;
+import com.Apothic0n.BiosphericalExpansion.api.biome.features.types.Basic3x2x3CubeFeature;
+import com.Apothic0n.EcosphericalExpansion.api.biome.features.types.Stemmed2x2x2CubeFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.SimpleBlockConfiguration;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public abstract class BioxFeatureRegistry {
     public static final DeferredRegister<Feature<?>> FEATURES = DeferredRegister.create(ForgeRegistries.FEATURES, BiosphericalExpansion.MODID);
+
+    public static final RegistryObject<Feature<SimpleBlockConfiguration>> BASIC_3X2X3_CUBE_FEATURE = FEATURES.register("basic_3x2x3_cube", () ->
+            new Basic3x2x3CubeFeature(SimpleBlockConfiguration.CODEC));
 
     public static void register(IEventBus eventBus) {
         FEATURES.register(eventBus);
