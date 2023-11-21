75b057df30a8a497190fc012b84c3a33a6a700dc
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2023-11-21 15:32:49 -0500

    - Recreated the Windswept Forest biome. - Recreated the Snowy Plains biome. - Recreated the Ice Spikes biome.

--- src/main/java/com/Apothic0n/BiosphericalExpansion/api/biome/features/BioxFeatureRegistry.java
+++ src/main/java/com/Apothic0n/BiosphericalExpansion/api/biome/features/BioxFeatureRegistry.java
@@ -2,8 +2,10 @@ package com.Apothic0n.BiosphericalExpansion.api.biome.features;
 
 import com.Apothic0n.BiosphericalExpansion.BiosphericalExpansion;
 import com.Apothic0n.BiosphericalExpansion.api.biome.features.types.Basic3x2x3CubeFeature;
+import com.Apothic0n.BiosphericalExpansion.api.biome.features.types.UnlimitedIceSpikeFeature;
 import com.Apothic0n.EcosphericalExpansion.api.biome.features.types.Stemmed2x2x2CubeFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.SimpleBlockConfiguration;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,6 +17,8 @@ public abstract class BioxFeatureRegistry {
 
     public static final RegistryObject<Feature<SimpleBlockConfiguration>> BASIC_3X2X3_CUBE_FEATURE = FEATURES.register("basic_3x2x3_cube", () ->
             new Basic3x2x3CubeFeature(SimpleBlockConfiguration.CODEC));
+    public static final RegistryObject<Feature<NoneFeatureConfiguration>> UNLIMITED_ICE_SPIKE_FEATURE = FEATURES.register("unlimited_ice_spike", () ->
+            new UnlimitedIceSpikeFeature(NoneFeatureConfiguration.CODEC));
 
     public static void register(IEventBus eventBus) {
         FEATURES.register(eventBus);
