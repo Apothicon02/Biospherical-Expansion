ea490c5609e549bfb2dca4b43a9f4922bb11fc16
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2023-11-29 23:53:44 -0500

    - Added the Glacial Caves biome. - Recreated the Geodeial Caves biome. - Replaced ore generation system. - Stripped the cherry trees in the Cherry Grove biome. - Stripped the spruce and oak trees in the Caldera biomes.

--- src/main/java/com/Apothic0n/BiosphericalExpansion/api/biome/features/BioxFeatureRegistry.java
+++ src/main/java/com/Apothic0n/BiosphericalExpansion/api/biome/features/BioxFeatureRegistry.java
@@ -1,8 +1,11 @@
 package com.Apothic0n.BiosphericalExpansion.api.biome.features;
 
 import com.Apothic0n.BiosphericalExpansion.BiosphericalExpansion;
+import com.Apothic0n.BiosphericalExpansion.api.biome.features.configurations.OreSpikeConfiguration;
 import com.Apothic0n.BiosphericalExpansion.api.biome.features.types.Basic3x2x3CubeFeature;
+import com.Apothic0n.BiosphericalExpansion.api.biome.features.types.OreSpikeFeature;
 import com.Apothic0n.BiosphericalExpansion.api.biome.features.types.UnlimitedIceSpikeFeature;
+import com.Apothic0n.EcosphericalExpansion.api.biome.features.configurations.VerticalBlobConfiguration;
 import com.Apothic0n.EcosphericalExpansion.api.biome.features.types.Stemmed2x2x2CubeFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
@@ -19,6 +22,8 @@ public abstract class BioxFeatureRegistry {
             new Basic3x2x3CubeFeature(SimpleBlockConfiguration.CODEC));
     public static final RegistryObject<Feature<NoneFeatureConfiguration>> UNLIMITED_ICE_SPIKE_FEATURE = FEATURES.register("unlimited_ice_spike", () ->
             new UnlimitedIceSpikeFeature(NoneFeatureConfiguration.CODEC));
+    public static final RegistryObject<Feature<OreSpikeConfiguration>> ORE_SPIKE_FEATURE = FEATURES.register("ore_spike", () ->
+            new OreSpikeFeature(OreSpikeConfiguration.CODEC));
 
     public static void register(IEventBus eventBus) {
         FEATURES.register(eventBus);
