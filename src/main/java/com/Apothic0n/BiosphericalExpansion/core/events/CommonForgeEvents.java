eab996843c53cc65178facad97769590153396df
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2024-02-27 21:11:03 -0500

    - Updated to 1.7.0. - Replaced ocean generation. - Terrain now forms continents. - Added rivers. - Removed noise caves. - Fixed dripstone being grayscale. - Added custom spawn location logic to make sure the world spawn is never above an ocean biome.

--- src/main/java/com/Apothic0n/BiosphericalExpansion/core/events/CommonForgeEvents.java
+++ src/main/java/com/Apothic0n/BiosphericalExpansion/core/events/CommonForgeEvents.java
@@ -3,16 +3,24 @@ package com.Apothic0n.BiosphericalExpansion.core.events;
 import com.Apothic0n.BiosphericalExpansion.BiosphericalExpansion;
 import com.Apothic0n.BiosphericalExpansion.core.objects.BioxBlocks;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.BiomeTags;
 import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.storage.ServerLevelData;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.level.LevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -20,7 +28,7 @@ import net.minecraftforge.fml.common.Mod;
 public class CommonForgeEvents {
 
     @SubscribeEvent
-    public static void itemUsed(PlayerInteractEvent.RightClickBlock event) {
+    public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
         Level pLevel = event.getLevel();
         BlockPos pPos = event.getHitVec().getBlockPos();
         BlockState pBlock = pLevel.getBlockState(pPos);
@@ -37,5 +45,25 @@ public class CommonForgeEvents {
                 }
             }
         }
+    }
+
+    @SubscribeEvent
+    public static void onCreateSpawnPosition(LevelEvent.CreateSpawnPosition event) {
+        boolean cancel = false;
+        if (event.getLevel() instanceof ServerLevel level && !level.dimensionType().hasCeiling()) {
+            for (int x = 0; x < 128000; x = x+128) {
+                for (int z = 0; z < 128000; z = z+128) {
+                    BlockPos spawnPos = new BlockPos(x, 56, z);
+                    if (!level.getBiome(spawnPos).is(BiomeTags.IS_OCEAN)) {
+                        event.getSettings().setSpawn(spawnPos.atY(level.getHeight(Heightmap.Types.WORLD_SURFACE, x, z)), 0.0F);
+                        cancel = true;
+                        x = 128000;
+                        z = 128000;
+                        break;
+                    }
+                }
+            }
+        }
+        event.setCanceled(cancel);
     }
 }
